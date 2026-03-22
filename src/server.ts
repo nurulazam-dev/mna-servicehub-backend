4d6d06e043df91a7c1a46529c3966f2577cb144d
mnaofficialbd <mna.official92@gmail.com>
2026-03-22 21:51:16 +0600

    server update

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
-import { Server } from "http";
 import app from "./app";
 import { envVars } from "./app/config/env";
 
-let server: Server;
-
 const main = async () => {
   try {
-    server = app.listen(envVars.PORT, () => {
+    app.listen(envVars.PORT, () => {
       console.log(`Server is running on PORT: ${envVars.PORT}`);
     });
   } catch (error) {
     console.error("Fail to start server", error);
   }
 };
+
+main();
