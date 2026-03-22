ad1442b48931d0f20a8194e7911e0f83649dd82a
mnaofficialbd <mna.official92@gmail.com>
2026-03-22 22:42:49 +0600

    auth schema primary setup

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 import { envVars } from "../config/env";
 
-const connectionString = envVars.DATABASE_URL; // Follow the (Better-Auth 8 No) step, then it will be work
+const connectionString = envVars.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
