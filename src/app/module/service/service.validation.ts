bd2b55e510e823f077ceb0c76a1fc5d858c8fad9
mnaofficialbd <mna.official92@gmail.com>
2026-03-24 23:10:07 +0600

    job-post validation create

--- src/app/module/service/service.validation.ts
+++ src/app/module/service/service.validation.ts
@@ -20,23 +20,6 @@ const createServiceZodSchema = z.object({
   isActive: z.boolean().optional(),
 });
 
-const updateServiceZodSchema = z.object({
-  description: z
-    .string()
-    .min(10, "Description must be at least 10 characters")
-    .max(500, "Description must not exceed 500 characters")
-    .optional(),
-
-  imageUrl: z
-    .string()
-    .url("Image URL must be a valid URL")
-    .nullable()
-    .optional(),
-
-  isActive: z.boolean().optional(),
-});
-
 export const ServiceValidation = {
   createServiceZodSchema,
-  updateServiceZodSchema,
 };
