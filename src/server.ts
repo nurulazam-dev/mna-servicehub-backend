44d906b89081cc885632450b53f7b45bd721355e
mnaofficialbd <mna.official92@gmail.com>
2026-03-24 11:42:09 +0600

    seed-admin create

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import app from "./app";
 import { envVars } from "./app/config/env";
+// import { seedAdmin } from "./app/utils/seedAdmin";
 
 const main = async () => {
   try {
+    // 1st admin seed
+    // await seedAdmin();
+
     app.listen(envVars.PORT, () => {
       console.log(`Server is running on PORT: ${envVars.PORT}`);
     });
