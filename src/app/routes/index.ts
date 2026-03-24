1e0d5257e177170caa9dc5373555b307c4017d84
mnaofficialbd <mna.official92@gmail.com>
2026-03-24 12:24:39 +0600

    job-posts route create

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { ServiceRoutes } from "../module/service/service.route";
+import { JobPostRoutes } from "../module/jobPost/jobPost.route";
 
 const router = Router();
 
 router.use("/auth", AuthRoutes);
 router.use("/services", ServiceRoutes);
+router.use("/job-posts", JobPostRoutes);
 
 export const IndexRoutes = router;
