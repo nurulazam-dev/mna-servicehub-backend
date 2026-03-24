77dce19b045df9cbfc251cc183884c6ac2957e0c
mnaofficialbd <mna.official92@gmail.com>
2026-03-24 23:54:04 +0600

    auth-route update

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -2,15 +2,31 @@ import { Router } from "express";
 import { AuthController } from "./auth.controller";
 import { checkAuth } from "../../middleware/checkAuth";
 import { UserRole } from "../../../generated/prisma/enums";
+import { validateRequest } from "../../middleware/validateRequest.ts";
+import { AuthValidation } from "./auth.validation";
 
 const router = Router();
 
-router.post("/register", AuthController.registerCustomer);
-router.post("/register-candidate", AuthController.registerJobCandidate);
+router.post(
+  "/register",
+  validateRequest(AuthValidation.registerCustomerZodSchema),
+  AuthController.registerCustomer,
+);
+
+router.post(
+  "/register-candidate",
+  validateRequest(AuthValidation.registerJobCandidateZodSchema),
+  AuthController.registerJobCandidate,
+);
 
 router.post("/verify-email", AuthController.verifyEmail);
 
-router.post("/login", AuthController.loginUser);
+router.post(
+  "/login",
+  validateRequest(AuthValidation.loginZodSchema),
+  AuthController.loginUser,
+);
+
 router.post(
   "/logout",
   checkAuth(
@@ -39,6 +55,7 @@ router.post("/forger-password", AuthController.forgetPassword);
 router.post("/reset-password", AuthController.resetPassword);
 router.post(
   "/change-password",
+  validateRequest(AuthValidation.changePasswordZodSchema),
   checkAuth(
     UserRole.ADMIN,
     UserRole.MANAGER,
